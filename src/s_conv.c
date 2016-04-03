ca0df53059929b580c0ec216be3a8b5f80a49929
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-04-03 22:17:42 +0200

    fix null %s

--- src/s_conv.c
+++ src/s_conv.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/03 02:11:07 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/04/03 20:17:13 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/04/03 22:17:00 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ t_list			*s_conv
 
 	(void)self;
 	(void)precision;
-	len = (x ? ft_strlen(x) : 7);
+	len = (x ? ft_strlen(x) - 1 : 6);
 	if (!(ret = ft_lstnew(NULL, len)))
 		return (NULL);
 	if (x)
 		ft_memcpy(ret->content, x, len);
 	else
-		ft_strcpy(ret->content, "(null)");
+		ft_memcpy(ret->content, "(null)", len);
 	return (ret);
 }
--- test/src/main.c
+++ test/src/main.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/30 19:52:26 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/04/03 20:30:58 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/04/03 22:17:33 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,5 @@
 
 int			main(void)
 {
-	int i = 1;
-	ft_printf("hello I'm ft_printf! I'll print 42 for you\n");
-	ft_printf("ftp: %#010x\n", 42);
-	printf("hello I'm printf! I'll print 42 for you\n");
-	printf("  p: %#010x\n", 42);
-	ft_printf("you should know that %+d is the maximum int\n", INT_MAX);
-	ft_printf("you should know that %d is the minimum int\n", INT_MIN);
-	ft_printf("%d - %d + %d * %d = 42\n", 12, 3, 12, 2);
+	ft_printf("{%s}\n", 0);
 }
