25fc4e5aac3a4e12c434ff1e91139ceefdac66a7
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-04-06 20:52:13 +0200

    fix %c

--- src/s_conv.c
+++ src/s_conv.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/03 02:11:07 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/04/06 20:43:13 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/04/06 20:51:14 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 t_list			*c_conv
 	(t_conv_spec *self, t_ctxt_spec *ctxt)
 {
-	if (ctxt->arg > 1)
+	char		arg[1];
+
+	if (self->size > 1)
 		return (wc_conv(self, ctxt));
-	else
-		return (ft_lstnew(&((char)ctxt->arg), 1));
+	arg[0] = (char)ctxt->arg;
+	return (ft_lstnew(arg, 1));
 }
 
 t_list			*s_conv
@@ -27,7 +29,7 @@ t_list			*s_conv
 	size_t		len;
 
 	(void)self;
-	if (ctxt->arg > 1)
+	if (self->size > 1)
 		wstr_conv(self, ctxt);
 	if (ctxt->prec)
 		len = ft_min(ctxt->prec, ft_strlen((char *)ctxt->arg));
