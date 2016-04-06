5a80c665a35992d6709d83e82ab5a4a906694cc4
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-04-06 21:17:15 +0200

    add include

---
+++ includes/libprintf_l_modifiers.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libprintf_l_modifiers.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/06 18:31:21 by qle-guen          #+#    #+#             */
+/*   Updated: 2016/04/06 19:35:00 by qle-guen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBPRINTF_L_MODIFIERS_H
+# define LIBPRINTF_L_MODIFIERS_H
+
+#include <stdint.h>
+
+typedef struct			s_l_modif_spec
+{
+	char				name[3];
+	size_t				size;
+}						t_l_modif_spec;
+
+static t_l_modif_spec	l_modifs_arr[] =
+{
+	{"hh", sizeof(char)},
+	{"h", sizeof(short)},
+	{"l", sizeof(long)},
+	{"ll", sizeof(long long)},
+	{"j", sizeof(intmax_t)},
+	{"t", sizeof(ptrdiff_t)},
+};
+
+#endif
